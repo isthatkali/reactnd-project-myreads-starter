743c1b5402f40a395562d0db42a91d878cb8f5d0
Kali <isthatkali@gmail.com>
2019-01-24 14:42:27 -0800

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,4 +3,12 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import { BrowserRouter } from 'react-router-dom'
+
+// wrap BrowserRouter around app to allow routing
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root')
+)
