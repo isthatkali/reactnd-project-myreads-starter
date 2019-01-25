28bc351cf792d745e6e2433c81d3c681488bb800
Kali <isthatkali@gmail.com>
2019-01-24 17:08:04 -0800

    main page broken down into shelf and book components

---
+++ src/components/Book.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+class Book extends React.Component {
+  componentDidMount() {
+    console.log(this);
+  }
+
+  render() {
+    return(
+    <li>
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover"
+            style={{ 
+              width: 128,
+              height: 193,
+              backgroundImage: `url(${this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : ''})`
+            }}
+           />
+          <div className="book-shelf-changer">
+            <select value={this.props.book.shelf || "None"}>
+              <option value="move" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{this.props.book.title || "N/A"}</div>
+        <div className="book-authors">{this.props.book.authors[0] || "N/A"}</div>
+      </div>
+    </li>
+
+    );
+  }
+}
+
+export default Book
